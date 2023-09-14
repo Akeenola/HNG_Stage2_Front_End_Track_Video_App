f5b85245b34f4f1fa6006137171e944dbd6c981f
github_akeenola <akinola.olabisi@gmail.com>
2023-09-14 07:20:20 -0700

    updates to video files

--- video_app/js/params.js
+++ video_app/js/params.js
@@ -1,3 +1,3 @@
-const sidebar = [{logo:"images/moviesLogo.jpeg",item:"Movies"},{logo:"images/home.jpeg",item:"Home"},
+const sidebarItem = [{logo:"images/moviesLogo.png",item:"Movies"},{logo:"images/home.jpeg",item:"Home"},
 {logo:"images/movies.jpeg",item:"Movies"},{logo:"images/tv_series.jpeg",item:"TV Series"},
 {logo:"images/upcoming.jpeg",item:"Upcoming"}];
