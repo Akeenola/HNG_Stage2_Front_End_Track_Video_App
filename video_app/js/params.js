2c757f200dc9e0752d6e0f1ab6676a1cb0f8f33d
github_akeenola <akinola.olabisi@gmail.com>
2023-09-13 21:43:04 -0700

    video app files

---
+++ video_app/js/params.js
@@ -0,0 +1,3 @@
+const sidebar = [{logo:"images/moviesLogo.jpeg",item:"Movies"},{logo:"images/home.jpeg",item:"Home"},
+{logo:"images/movies.jpeg",item:"Movies"},{logo:"images/tv_series.jpeg",item:"TV Series"},
+{logo:"images/upcoming.jpeg",item:"Upcoming"}];
