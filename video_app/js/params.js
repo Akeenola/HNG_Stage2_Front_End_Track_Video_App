10af21a46aac013df7fb789e3045654624b58aa0
github_akeenola <akinola.olabisi@gmail.com>
2023-09-14 11:53:02 -0700

    unique key added to each child

--- video_app/js/params.js
+++ video_app/js/params.js
@@ -1,3 +1,13 @@
-const sidebarItem = [{logo:"images/moviesLogo.png",item:"Movies"},{logo:"images/home.jpeg",item:"Home"},
-{logo:"images/movies.jpeg",item:"Movies"},{logo:"images/tv_series.jpeg",item:"TV Series"},
+const sidebarItem = [{id:"1",logo:"images/moviesLogo.png",item:"Movies"},{id:"2",logo:"images/home.jpeg",item:"Home"},
+{id:"3",logo:"images/movies.jpeg",item:"Movies"},{id:"4",logo:"images/tv_series.jpeg",item:"TV Series"},
 {logo:"images/upcoming.jpeg",item:"Upcoming"}];
+var videoPanl = [{title:"Top Gun",year:"2022",rating:"PG-13",length:"2 hours",family:"Action",genre:"Drama",
+short_story:"After 30 years Maventher is still pushing the envelope",
+role1:{dir1:"Director",name1:"Joseph Kosuskii"},role2:{wri1:"writers",name2:"Jim Kash, tach Epoch"},
+role3:{start1:"Starts",name3:"Tom Cruise,Jenof Cousin"}},
+
+{title:"Top Gun",year:"2022",rating:"PG-13",length:"2 hours",family:"Action",genre:"Drama",
+short_story:"After 30 years Maventher is still pushing the envelope",
+role1:{dir1:"Director",name1:"Joseph Kosuskii"},role2:{wri1:"writers",name2:"Jim Kash, tach Epoch"},
+role3:{start1:"Starts",name3:"Tom Cruise,Jenof Cousin"}}
+]
