c46e9a28a85a7b5d70d040fc1f99d331bc06534b
github_akeenola <akinola.olabisi@gmail.com>
2023-09-14 14:38:59 -0700

    package.json added

--- video_app/js/params.js
+++ video_app/js/params.js
@@ -6,8 +6,8 @@ short_story:"After 30 years Maventher is still pushing the envelope",
 role1:{dir1:"Director",name1:"Joseph Kosuskii"},role2:{wri1:"writers",name2:"Jim Kash, tach Epoch"},
 role3:{start1:"Starts",name3:"Tom Cruise,Jenof Cousin"}},
 
-{title:"Top Gun",year:"2022",rating:"PG-13",length:"2 hours",family:"Action",genre:"Drama",
-short_story:"After 30 years Maventher is still pushing the envelope",
+{title:"Gorrilla",year:"2022",rating:"PG-13",length:"2 hours",family:"Action",genre:"Drama",
+short_story:"After 30 years Maventher is still pushing the envelope. A predicate is a function that always returns a Boolean value. Array.filter invokes this predicate once for every item in the array. That item is passed to the predicate as anargument, and the return value is used to decide if that item will be added to the new array.",
 role1:{dir1:"Director",name1:"Joseph Kosuskii"},role2:{wri1:"writers",name2:"Jim Kash, tach Epoch"},
 role3:{start1:"Starts",name3:"Tom Cruise,Jenof Cousin"}}
 ]
