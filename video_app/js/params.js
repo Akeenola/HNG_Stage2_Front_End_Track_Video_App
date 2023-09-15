069ceba8d49b398ddbf7100494fb4f517b2bd2ce
github_akeenola <akinola.olabisi@gmail.com>
2023-09-14 17:00:59 -0700

    add advert photo

--- video_app/js/params.js
+++ video_app/js/params.js
@@ -1,6 +1,8 @@
 const sidebarItem = [{id:"1",logo:"images/moviesLogo.png",item:"Movies"},{id:"2",logo:"images/home.jpeg",item:"Home"},
 {id:"3",logo:"images/movies.jpeg",item:"Movies"},{id:"4",logo:"images/tv_series.jpeg",item:"TV Series"},
 {logo:"images/upcoming.jpeg",item:"Upcoming"}];
+
+
 var videoPanl = [{title:"Top Gun",year:"2022",rating:"PG-13",length:"2 hours",family:"Action",genre:"Drama",
 short_story:"After 30 years Maventher is still pushing the envelope",
 role1:{dir1:"Director",name1:"Joseph Kosuskii"},role2:{wri1:"writers",name2:"Jim Kash, tach Epoch"},
@@ -11,3 +13,8 @@ short_story:"After 30 years Maventher is still pushing the envelope. A predicate
 role1:{dir1:"Director",name1:"Joseph Kosuskii"},role2:{wri1:"writers",name2:"Jim Kash, tach Epoch"},
 role3:{start1:"Starts",name3:"Tom Cruise,Jenof Cousin"}}
 ]
+
+const photoAdvert = [{id:"1",logo:"images/movie1.png",item:"Gorrilla War"},{id:"2",logo:"images/movie2.jpeg",item:"Disney Panda"},
+{id:"3",logo:"images/movie3.jpeg",item:"Zaka the zulu"},{id:"4",logo:"images/movie4.jpeg",item:"Queen in the Den"},
+{logo:"images/movie5.png",item:"The connectives"},,{id:"5",logo:"images/movie6.jpeg",item:"Endless Game"},{id:"7",logo:"images/movie7.png",item:"Fugitive"}
+,{id:"8",logo:"images/movie8.jpeg",item:"Gang War"}];
